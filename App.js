8c259a79545cd48e4f284a0da622fd8a4aef58ca
Pedro Luiz Aroma FIlho <pedroaroma@pop-os.localdomain>
2020-04-05 03:11:52 -0300

    adicionado Componente Comments

--- App.js
+++ App.js
@@ -6,10 +6,23 @@ import Post from './src/components/Post'
 
 export default class App extends Component {
 	render() {
+
+		const comments = [
+			{
+			nickname: 'Dummy 1',
+			comment: 'DAHORA'
+		},
+		{
+			nickname: 'Dummy 2',
+			comment: 'BEM LOCO'
+		}
+		]
 		return (
 			<View style={{flex: 1}}>
 				<Header />
-				<Post image={require('./assets/imgs/fence.jpg')} />
+				<Post image={require('./assets/imgs/fence.jpg')} 
+					comments={comments}
+				/>
 			</View>
     	)
 	}
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {StyleSheet, View, Image, Dimensions } from 'react-native'
 import Author from './Author'
+import Comments from './Comments'
 
 class Post extends Component {
     render(){
@@ -8,6 +9,7 @@ class Post extends Component {
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image}></Image>
                 <Author email='aroma.pedro@gmail.com' nickname='Pedro Luiz Aroma Filho' />
+                <Comments comments={this.props.comments} />
             </View>
         )
     }
