386330dbbaf55c8189db439cdc915b1c905c0a31
Pedro Luiz Aroma FIlho <pedroaroma@pop-os.localdomain>
2020-04-05 02:51:25 -0300

    adicionado componente Post

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { } from 'react-native';
+import { View, Text} from 'react-native';
+
 import Header from './src/components/Header'
+import Post from './src/components/Post'
 
 export default class App extends Component {
 	render() {
 		return (
-			<Header />
+			<View style={{flex: 1}}>
+				<Header />
+				<Post image={require('./assets/imgs/fence.jpg')} />
+			</View>
     	)
 	}
 }
